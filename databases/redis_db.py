b4dc9e9b5f7386f5d75351c92ff79ab09ead85d1
Bùi Ngọc Dũng <fcb2124@gmail.com>
2024-10-04 12:45:30 +0700

    [Fix] remove decode reponse

--- databases/redis_db.py
+++ databases/redis_db.py
@@ -5,8 +5,7 @@ class RedisStorage:
     def __init__(self, host='75.119.149.223', port=6379, redis_password='@2Vietnam', db=0):
         """Initialize the RedisStorage class and connect to Redis."""
         self.redis_client = redis.Redis(host=host, port=port, 
-        password=redis_password,
-        decode_responses=True, db=db)
+        password=redis_password, db=db)
 
     def store_face_data(self, names, encodings, backbone):
         """Store face encodings and names in Redis."""
@@ -17,8 +16,9 @@ class RedisStorage:
         """Load face encodings and names from Redis."""
         self.known_face_names = []
         self.known_face_encodings = []
-
+        print(backbone + "_face_data")
         face_data = self.redis_client.hgetall(backbone + "_face_data")
+        
         for name, encoding_bytes in face_data.items():
             self.known_face_names.append(name.decode('utf-8'))
             self.known_face_encodings.append(np.frombuffer(encoding_bytes, dtype=np.float32))
